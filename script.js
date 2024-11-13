97bc737f635a398363da0c79e9e72c914de3e36d
Mahbubrm9 <mahbubrm9@gmail.com>
2024-11-13 21:29:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    // autoplay: {
+    //     delay: 5000,
+    //   },
+      loop:true,
+    spaceBetween: 30,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+ 
